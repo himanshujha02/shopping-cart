be34b0ba55c842345ddb807924b171672333f37d
Himanshu <himanshujha116@gmail.com>
2024-02-20 16:35:03 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,67 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.webp";
+import product7 from "./assets/products/7.webp";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 999.0,
+    productImage: product1,
+    brand:"Iphone",
+  },
+  {
+    id: 2,
+    productName: "Samsung f34",
+    price: 599.0,
+    productImage: product2,
+    brand:"Samsung",
+  },
+  {
+    id: 3,
+    productName: "Oneplus",
+    price: 699.0,
+    productImage: product3,
+    brand:"Oneplus",
+  },
+  {
+    id: 4,
+    productName: "Redmi 12",
+    price: 228.0,
+    productImage: product4,
+    brand:"Redmi",
+  },
+  {
+    id: 5,
+    productName: "Motoroala F32",
+    price: 228.99,
+    productImage: product5,
+    brand:"Motorola",
+  },
+  {
+    id: 6,
+    productName: "Samsung S23 Ultra",
+    price: 799,
+    productImage: product6,
+    brand:"Samsung"
+  },
+  {
+    id: 7,
+    productName: "Samsung lite",
+    price: 300,
+    productImage: product7,
+    brand:"Samsung"
+  },
+  {
+    id: 8,
+    productName: "",
+    price: 400.0,
+    productImage: product8,
+    
+  },
+];
